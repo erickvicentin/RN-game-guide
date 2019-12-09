5309f3b02505f6a9fd909f47f2cf80e209fdc629
erickvicentin <erickvicentin14@hotmail.com>
2019-12-09 07:10:08 -0300

    Agregue a App.js la pantalla de inicio del juego

--- App.js
+++ App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import style from './styles/Main';
 import Header from './components/Header';
+import StartScreen from './screens/StartScreen';
 
 export default function App() {
 
   return (
     <View style={style.screen}>
       <Header title='Adivinar un numero'/>
+      <StartScreen/>
     </View>
   );
 
