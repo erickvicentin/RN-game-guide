9b1d691c370f8921c4bbd373633cdf0fd89f1b20
erickvicentin <erickvicentin14@hotmail.com>
2019-12-09 07:50:16 -0300

    se agrega tarjeta para ingresar datos con titulo

--- screens/StartScreen.js
+++ screens/StartScreen.js
@@ -1,13 +1,26 @@
 import React from 'react';
-import { View, Text } from "react-native";
+import { View, Text, TextInput, Button   } from "react-native";
 import style from '../styles/StartScreen';
 
 const StartScreen = props => {
     return (
         <View style={style.screen}>
-            <Text>
-                The Game Screen!
-            </Text>
+            <Text style={style.title}>Comenzar un nuevo juego!</Text>
+            <View style={style.inputContainer}>
+                <Text>Seleccione un numero</Text>
+                <TextInput style={style.inputText}/>
+                <View style={style.buttonContainer}>
+                    
+                    <View style={style.button}>
+                        <Button title="Reiniciar" color='red' onPress={() => {}}/>
+                    </View>
+                    
+                    <View style={style.button}>
+                        <Button title="Confirmar" onPress={() => {}}/>
+                    </View>
+
+                </View>
+            </View>
         </View>
     );
 };
