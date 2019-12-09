d6236b74e14386cee7eb0f1f9c18c1559f32036b
erickvicentin <erickvicentin14@hotmail.com>
2019-12-09 07:51:24 -0300

    se crean estilos para los componentes de la pantalla StartScreen

--- styles/StartScreen.js
+++ styles/StartScreen.js
@@ -5,6 +5,35 @@ const style = StyleSheet.create ({
         flex: 1,
         padding: 10,
         alignItems: 'center',
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '100%',
+        paddingHorizontal: 15   
+    },
+    button: {
+        width: '40%'
+    },
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: 'center',
+        shadowColor: 'black',
+        shadowOffset: { width: 0 , height: 2 },
+        shadowRadius: 6,
+        shadowOpacity: 0.26,
+        backgroundColor: 'white',
+        elevation: 5,
+    },
+    title: {
+        fontSize: 20,
+        marginVertical: 10
+    },
+    inputText: {
+        width: '80%',
+        marginBottom: 20,
+        textAlign: 'center',
     }
 });
 
