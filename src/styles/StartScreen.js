6e13e3ab36a0e82b2968cad0c0e8dfe1a509e61c
erickvicentin <erickvicentin14@hotmail.com>
2019-12-10 02:35:04 -0300

    modificacion de estilos pantalla principal

--- src/styles/StartScreen.js
+++ src/styles/StartScreen.js
@@ -9,27 +9,25 @@ const style = StyleSheet.create ({
     buttonContainer: {
         flexDirection: 'row',
         justifyContent: 'space-between',
-        width: '100%',
-        paddingHorizontal: 15   
+        width: "100%",
+        paddingHorizontal: 15
+
     },
     button: {
         width: 95
-    },
-    inputContainer: {
-        width: 300,
-        maxWidth: '80%',
-        alignItems: 'center',
-        
     },
     title: {
         fontSize: 20,
         marginVertical: 10
     },
-    inputText: {
-        width: '80%',
-        marginBottom: 20,
-        textAlign: 'center',
-        marginVertical: 20
+    inputContainer: {
+        width: 300,
+        maxWidth: '80%',
+        alignItems: "center"
+    },
+    input: {
+        width: 50,
+        textAlign: 'center'
     }
 });
 
