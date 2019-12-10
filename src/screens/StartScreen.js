9cf8836d7e58554fa8e4fc53d88ad063f6f61f52
erickvicentin <erickvicentin14@hotmail.com>
2019-12-10 02:33:06 -0300

    se modifica pantalla inicio

--- src/screens/StartScreen.js
+++ src/screens/StartScreen.js
@@ -1,27 +1,43 @@
 import React from 'react';
-import { View, Text, TextInput, Button   } from 'react-native';
+import { View, Text, Button   } from 'react-native';
 import style from '../styles/StartScreen';
 import Card from '../components/Card';
+import Colors from '../constants/colors';
+import Input from '../components/Input';
 
 const StartScreen = props => {
     return (
         <View style={style.screen}>
-            <Text style={style.title}>'Pantalla comenzar'</Text>
-            <Card style={style.inputContainer}>   
+            
+            <Text style={style.title}>Comenzar el juego</Text>
+
+            <Card style={style.inputContainer}>  
+            
                 <Text>Seleccione un numero</Text>
-                <TextInput style={style.inputText} placeholder='Ingrese un numero'/>
-                <View style={style.buttonContainer}>
+                
+                <Input 
+                    style = {style.input} 
+                    blurOnSumbit 
+                    autoCapitalize = 'none' 
+                    autoCorrect = { false }
+                    keyboardType = "number-pad"
+                    maxLength={2}
+                    />
+               
+                 <View style={style.buttonContainer}>
                     
                     <View style={style.button}>
-                     <Button title="Reiniciar" color='black' onPress={() => {}} />
+                     <Button title="Reiniciar" color={Colors.accent} onPress={() => {}} />
                     </View>
 
                     <View style={style.button}>
-                    <Button title="Aceptar" color='black' onPress={() => {}} />
+                    <Button title="Aceptar" color={Colors.primary} onPress={() => {}} />
                     </View>
 
                 </View>
+
             </Card>
+
         </View>
     );
 };
